0bf2834ffac3a40dbed90fe8edde0866f958df65
Pavel Sapezhko <me@weastur.com>
2019-09-21 21:54:44 +0300

    Add argument parsers

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+#!/usr/bin/env python3
+
 import argparse
+
+
+class Platform:
+    CODEFORCES = 'codeforces'
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser.add_argument(
+        '--platform',
+        help='contest platform(only codeforces supported for now)',
+        default=Platform.CODEFORCES,
+        choices=(
+            Platform.CODEFORCES,
+        ),
+    )
+    parser.add_argument(
+        '-c',
+        '--contest',
+        help='contest number',
+        type=int,
+    )
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
